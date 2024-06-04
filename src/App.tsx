e15d8d847fa90001803adf44818bad539e63823f
Gabriel Lopes Rodrigues <gabriellopes@outlook.com>
2024-06-03 21:16:55 -0300

    feat: add styled components, set theme colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { ThemeProvider } from "styled-components";
+import { Home } from "./pages/Home";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <Home />
+    </ThemeProvider>
   );
 }
