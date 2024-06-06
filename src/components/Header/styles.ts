19193cd47bbd61b457847c949772c188b3cf8e14
Gabriel Lopes Rodrigues <gabriellopes@outlook.com>
2024-06-05 22:59:53 -0300

    feat: markup and style for Header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+import backgroundImg from "../../assets/Cover.png";
+
+export const HeaderContainer = styled.header`
+  margin: 0;
+  background: url(${backgroundImg});
+
+  width: 100%;
+  height: 18.5rem;
+`
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,53 @@
+import { Header } from "../../components/Header";
+
 export function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <Header />
+      <div>
+        <img src="" alt="" />
+        <div>
+          <h1>Cameron Williamson</h1>
+          <p>
+            Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
+            viverra massa quam dignissim aenean malesuada suscipit. Nunc,
+            volutpat pulvinar vel mass.
+          </p>
+
+          <div>
+            <span>cameronwll</span>
+            <span>Rocketseat</span>
+            <span>32 seguidores</span>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <div>
+            <h2>JavaScript data types and data structures</h2>
+            <span>Há 1 dia</span>
+          </div>
+          <p>
+            Programming languages all have built-in data structures, but these
+            often differ from one language to another. This article attempts to
+            list the built-in data structures available in JavaScript and what
+            properties they have.
+          </p>
+        </div>
+        <div>
+          <div>
+            <h2>JavaScript data types and data structures</h2>
+            <span>Há 1 dia</span>
+          </div>
+          <p>
+            Programming languages all have built-in data structures, but these
+            often differ from one language to another. This article attempts to
+            list the built-in data structures available in JavaScript and what
+            properties they have.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
