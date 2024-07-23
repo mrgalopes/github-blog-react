6ea7af246d90f9990096aa973cce0ed454cdbc68
Gabriel Lopes Rodrigues <gabriellopes@outlook.com>
2024-07-22 21:26:09 -0300

    fix: makes background no repeat and cover

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,6 +5,8 @@ import backgroundImg from "../../assets/Cover.png";
 export const HeaderContainer = styled.header`
   margin: 0;
   background: url(${backgroundImg});
+  background-repeat: no-repeat;
+  background-size: cover;
 
   width: 100%;
   height: 18.5rem;
