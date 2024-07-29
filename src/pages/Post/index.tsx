705fca3cb8c671336b06e0c1a1e4b5a0ff30ad7d
Gabriel Lopes Rodrigues <gabriellopes@outlook.com>
2024-07-29 20:38:14 -0300

    feat: add support for reading markdown

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 // import { useParams } from "react-router-dom";
 import { PostCard, PostLink, PostText } from "./styles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -9,9 +11,22 @@ import {
   faUserGroup,
 } from "@fortawesome/free-solid-svg-icons";
 import { TextM, TitleL } from "../../styles/mixins";
+import Markdown from "react-markdown";
+
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+import demoPost from "./demo-post.md";
+import { useEffect, useState } from "react";
 
 export function Post() {
   // const { slug } = useParams();
+  const [markdown, setMarkdown] = useState("");
+  useEffect(() => {
+    fetch(demoPost)
+      .then((res) => res.text())
+      .then((text) => setMarkdown(text));
+  }, []);
 
   return (
     <>
@@ -43,6 +58,31 @@ export function Post() {
           </div>
         </PostText>
       </PostCard>
+
+      <div>
+        <Markdown
+          components={{
+            code(props) {
+              const { children, className, ...rest } = props;
+              const match = /language-(\w+)/.exec(className || "");
+              return match ? (
+                <SyntaxHighlighter
+                  PreTag="div"
+                  children={String(children).replace(/\n$/, "")}
+                  language={match[1]}
+                  style={dark}
+                />
+              ) : (
+                <code {...rest} className={className}>
+                  {children}
+                </code>
+              );
+            },
+          }}
+        >
+          {markdown}
+        </Markdown>
+      </div>
     </>
   );
 }
